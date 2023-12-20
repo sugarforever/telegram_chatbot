93801ccb9a5604242c13862e57ca8163e37344e5
sugarforever <william.1022@gmail.com>
2023-12-20 18:12:40 +0000

    first version

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import telegram
+import os
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+APP_URL = os.getenv("APP_URL")
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post(f"/{TELEGRAM_TOKEN}")
+async def respond(request: Request):
+    body = await request.json()
+    print(body)
+    return "ok"
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+   s = bot.setWebhook(f"{APP_URL}/{TELEGRAM_TOKEN}")
+   if s:
+       return "webhook setup ok"
+   else:
+       return "webhook setup failed"
